9f838718e9c65bb09d93adacb7e3d96c7d7c43d4
nivash77 <151871666+nivash77@users.noreply.github.com>
2024-08-21 19:40:24 +0530

    Add files via upload

---
+++ testingapp.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import google.generativeai as genai
+st.title("MY AI APP")
+genai.configure(api_key="AIzaSyBcwMbtKtUFjGoXagqdeIOmZ-iO4t4sq30")
+text = st.text_input("Enter a Question:")
+if st.button("Clike me"):
+    if text.strip():
+        model = genai.GenerativeModel('gemini-pro')
+        chat = model.start_chat(history=[])
+        response = chat.send_message(text)
+        st.html("<p><Strong> Answer for above question<Strong></p>")
+        st.write(response.text)
+    else:
+        st.info("Please enter a question")
+    
